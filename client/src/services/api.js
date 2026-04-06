bb68136a83fbd0ce8a922957912911ca1f3051b9
user172005 <divyak172005@gmail.com>
2026-04-06 19:26:14 +0530

    fix api base url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({ baseURL: (import.meta.env.VITE_SERVER_URL || '') + '/api' });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('wp_token');
